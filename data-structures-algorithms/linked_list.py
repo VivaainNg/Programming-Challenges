b6239b986fba47d3a61332f88c8532e6a6e5b19c
VivaainNg <123ulugubang@gmail.com>
2024-08-27 22:10:57 +0800

    Added file for data structs (LL).

---
+++ data-structures-algorithms/linked_list.py
@@ -0,0 +1,19 @@
+
+class LinkedList:
+    """Basic implementation of Linked List (LL)."""
+
+    def __init__(self):
+        pass
+
+
+class Node:
+    """Representation of each Node within a LL."""
+
+
+    def __init__(self, value, pointer):
+        self.value = value
+        self.pointer = pointer
+        
+
+if __name__ == "__main__":
+    pass
