f1df793013fca4beb5c5f29abfe7103a375c1d41
VivaainNg <123ulugubang@gmail.com>
2024-09-25 18:11:29 +0800

    Initial subdire structure for AOC 2015

--- advent-of-code/python/test/test_2015.py
+++ advent-of-code/python/test/test_2015.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 import unittest
 
 from year2015.day1 import solve as solve1
@@ -10,6 +12,33 @@ class TestAOC2015(unittest.TestCase):
     Entire test suite of year 2015.
     """
 
-    def test_1(self):
-        """Day 1 test case"""
-        self.assertEqual(23, solve1())
+    PATH_TO_INPUT_DIRECTORY = "../../inputs/2015"
+
+    def test_1_1(self):
+        """Day 1 Part 1 test case"""
+
+        file_name = "day1_1.txt"
+
+        current_path = os.path.dirname(__file__)
+
+        input_path = os.path.join(current_path, self.PATH_TO_INPUT_DIRECTORY, file_name)
+
+        ans = 0
+
+        if os.path.exists(input_path):
+            f = open(input_path)
+            data = f.read()
+            ans = solve1(data)
+            f.close()
+
+
+        self.assertEqual(0, solve1("(())"))
+        self.assertEqual(0, solve1("()()"))
+        self.assertEqual(3, solve1("((("))
+        self.assertEqual(3, solve1("(()(()("))
+        self.assertEqual(3, solve1("))((((("))
+        self.assertEqual(-1, solve1("())"))
+        self.assertEqual(-1, solve1("))("))
+        self.assertEqual(-3, solve1(")))"))
+        self.assertEqual(-3, solve1(")())())"))
+        self.assertEqual(280, solve1(data))
--- advent-of-code/python/year2015/day1.py
+++ advent-of-code/python/year2015/day1.py
@@ -1,5 +1,16 @@
 from __future__ import annotations
 
 
-def solve():
-    return 999
+def solve(data: str) -> int:
+    """Simply return values based on input string data."""
+
+    ans = 0
+    data = data.strip()
+
+    for i in data:
+        if i == "(":
+            ans += 1
+        elif i == ")":
+            ans -= 1
+
+    return ans
